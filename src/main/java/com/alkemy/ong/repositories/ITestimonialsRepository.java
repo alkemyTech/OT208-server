fc6ae3e7d922ba6a6be37d36a76643c6bf11e845
Nestor Agredo <nagredo@bancolombia.com.co>
2022-05-21 10:25:59 -0500

    -Update Testimonials repository

--- src/main/java/com/alkemy/ong/repositories/ITestimonialsRepository.java
+++ src/main/java/com/alkemy/ong/repositories/ITestimonialsRepository.java
@@ -2,11 +2,13 @@ package com.alkemy.ong.repositories;
 
 import com.alkemy.ong.models.TestimonialsEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author nagredo
  * @project OT208-server
  * @class TestimonialsRepository
  */
+@Repository
 public interface ITestimonialsRepository extends CrudRepository<TestimonialsEntity, Long> {
 }
