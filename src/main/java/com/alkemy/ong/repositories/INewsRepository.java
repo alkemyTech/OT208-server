0d5c7bd11e5fe21fbf89f1052bbced3cb9dd09c2
Mateo Ospina <mateoospinadesarrollo@gmail.com>
2022-05-23 18:55:50 -0500

    Corrección tipo de dato id en el repositorio

--- src/main/java/com/alkemy/ong/repositories/INewsRepository.java
+++ src/main/java/com/alkemy/ong/repositories/INewsRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface INewsRepository extends JpaRepository<NewsEntity,Long> {
+public interface INewsRepository extends JpaRepository<NewsEntity,String> {
 }
