8a6c32b5147323027fe3bb8ddb4336c589bb8c5c
Mateo Ospina <mateoospinadesarrollo@gmail.com>
2022-05-23 20:03:56 -0500

    Rename NewsRepository

--- src/main/java/com/alkemy/ong/repositories/NewsRepository.java
+++ src/main/java/com/alkemy/ong/repositories/NewsRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface INewsRepository extends JpaRepository<NewsEntity,String> {
+public interface NewsRepository extends JpaRepository<NewsEntity,String> {
 }
