1f69ac56e208342e19d361a6c0cff20412d455b3
acamus79 <adrianedgardocamus@gmail.com>
2022-05-26 13:19:26 -0300

    OT208-24
    Changed implementations of merged classes, added saveUser method, and two exception classes

--- src/main/java/com/alkemy/ong/repositories/IRoleRepository.java
+++ src/main/java/com/alkemy/ong/repositories/IRoleRepository.java
@@ -3,7 +3,12 @@ package com.alkemy.ong.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.alkemy.ong.models.RoleEntity;
+import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IRoleRepository extends JpaRepository<RoleEntity, String> {
+    
+    Optional<RoleEntity> findByName(String name);
 	
 }
--- src/main/java/com/alkemy/ong/services/impl/UserServiceImpl.java
+++ src/main/java/com/alkemy/ong/services/impl/UserServiceImpl.java
@@ -1,48 +1,95 @@
-/*
- * 
- */
 package com.alkemy.ong.services.impl;
 
 import com.alkemy.ong.dto.UserDTO;
-import com.alkemy.ong.exeptions.EmailExistsException;
+import com.alkemy.ong.dtos.request.UserLogindto;
+import com.alkemy.ong.models.RoleEntity;
 import com.alkemy.ong.models.UserEntity;
+import com.alkemy.ong.repositories.IRoleRepository;
 import com.alkemy.ong.repositories.IUserRepository;
 import com.alkemy.ong.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Arrays;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService { 
 
-public class UserServiceImpl implements UserService{
-    
-    @Autowired
-    private IUserRepository userRepository;
-    
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final IUserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final IRoleRepository roleRepository;
 
+    public UserDetails login(UserLogindto userLoginDto) {
+        UserDetails userDetails;
+
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                userLoginDto.getEmail(), userLoginDto.getPassword()));
+        userDetails = (UserDetails) authentication.getPrincipal();
+
+        return userDetails;
+
+    }
+
+    public Optional<UserEntity> findByEmail(String email) {
+        userRepository.findByEmail(email);
+        return null;
+    }
+
+    /**
+     * 
+     * @param userDTO
+     * @return
+     * @throws EmailExistsException 
+     */
     @Override
-    public UserEntity saveUser(UserDTO userDTO) throws EmailExistsException{
-        
-        if(emailExist(userDTO.getEmail())){
+    public UserEntity saveUser(UserDTO userDTO) throws EmailExistsException {
+
+        if (emailExist(userDTO.getEmail())) {
             throw new EmailExistsException(
-            "There is an account with that email adress:" + userDTO.getEmail());
+                    "There is an account with that email adress:" + userDTO.getEmail());
         }
-               
+
         UserEntity userEntity = new UserEntity();
         userEntity.setFirstName(userDTO.getFirstName());
         userEntity.setLastName(userDTO.getLastName());
         userEntity.setEmail(userDTO.getEmail());
         userEntity.setPassword(passwordEncoder.encode(userDTO.getPassword()));
-        //userEntity.setPhoto();
-        //userEntity.setRole(); // Falta Resolver Rol
+        userEntity.setPassword(userDTO.getPassword());
+        userEntity.setRoleIds(Arrays.asList(roleExist("USER")));
         userEntity = this.userRepository.save(userEntity);
 
         return userEntity;
     }
     
-        private boolean emailExist(String email){
+    /**
+     * 
+     * @param email
+     * @return 
+     */
+    private boolean emailExist(String email) {
         return userRepository.findByEmail(email).isPresent();
     }
     
-    
+    /**
+     * 
+     * @param role
+     * @return
+     * @throws RoleExistException 
+     */
+    private RoleEntity roleExist(String role) throws RoleExistException {
+
+        if (roleRepository.findByName(role).isPresent())  {
+            return roleRepository.findByName("USER").get();
+        } else {
+            throw new RoleExistException(
+                "Rol dont's exist:" + role);
+        }
+    }
 }
