bdebac2a7d7bd76580251b2fc7ebeb36a5af627f
acamus79 <adrianedgardocamus@gmail.com>
2022-05-26 22:28:04 -0300

    Update EmailServiceImpl.java

--- src/main/java/com/alkemy/ong/services/impl/EmailServiceImpl.java
+++ src/main/java/com/alkemy/ong/services/impl/EmailServiceImpl.java
@@ -1,12 +1,60 @@
-/*
- * 
- */
 package com.alkemy.ong.services.impl;
 
-/**
- *
- * @author acamus
- */
-public class EmailServiceImpl {
-    
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.alkemy.ong.services.EmailService;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class EmailServiceImpl implements EmailService {
+
+    private final SendGrid sendGrid;
+
+    @Value("${myemail}")
+    private String myEmail;
+
+    @Override
+    public Response sendEmailRegister(String emailUser) throws IOException {
+        String subject = "¡¡Welcome!!";
+        String emailContent = "We welcome you to our platform, thank you for registering!!";
+        String emailType = "text/plain";
+
+        return sendEmail(emailUser, subject, emailContent, emailType);
+    }
+
+    @Override
+    public Response sendEmailContactForm(String emailUser) throws IOException {
+        String subject = "Thank you for contacting us.";
+        String emailContent = "Thank you for contacting us. We will be reading your request and we will contact you";
+        String emailType = "text/plain";
+
+        return sendEmail(emailUser, subject, emailContent, emailType);
+    }
+
+    private Response sendEmail(String emailUser, String emailSubject, String emailContent, String emailType)
+            throws IOException {
+        Email from = new Email(myEmail);
+        String subject = emailSubject;
+        Email to = new Email(emailUser);
+        Content content = new Content(emailType, emailContent);
+        Mail mail = new Mail(from, subject, to, content);
+        Request request = new Request();
+        request.setMethod(Method.POST);
+        request.setEndpoint("mail/send");
+        request.setBody(mail.build());
+
+        return sendGrid.api(request);
+    }
 }
