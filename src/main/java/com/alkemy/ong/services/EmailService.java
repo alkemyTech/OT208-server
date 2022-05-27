b985c9eaade2927468765800665753a494f7e3d9
Martin <iriartemartin2@gmail.com>
2022-05-27 16:03:59 -0300

    Se trackea EmailService eliminado.

---
+++ src/main/java/com/alkemy/ong/services/EmailService.java
@@ -0,0 +1,12 @@
+package com.alkemy.ong.services;
+
+import com.sendgrid.Response;
+import java.io.IOException;
+
+public interface EmailService {
+	
+	 public Response sendEmailRegister(String emailUser) throws IOException;
+	 
+	 public Response sendEmailContactForm(String emailUser) throws IOException;
+
+}
