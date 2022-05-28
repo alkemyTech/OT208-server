2e7bd0ec5207995c41e2c3b5f22a42eaac50b01e
Martin <iriartemartin2@gmail.com>
2022-05-27 22:25:42 -0300

    Se agrega se agrega repositorio base  'IBasicRepository' con los metodos necesarios para todos los servicios.

--- src/main/java/com/alkemy/ong/services/impl/BasicServiceImpl.java
+++ src/main/java/com/alkemy/ong/services/impl/BasicServiceImpl.java
@@ -5,29 +5,42 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alkemy.ong.repositories.IBasicRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class BasicServiceImpl <T, ID, R extends JpaRepository<T, ID>>{
-	
+public class BasicServiceImpl<T, ID, R extends IBasicRepository<T, ID>> {
+
 	protected final R repositorio;
 
 	public T save(T t) {
 		return repositorio.save(t);
 	}
 
-	public Optional<T> findById(ID id) {
+	public Optional<T> findByIdTrue(ID id) {
 		return repositorio.findById(id);
+	}
+
+	public Optional<T> findByIdDeleteFalse(ID id) {
+		return repositorio.findByIdAndSoftDeleteFalse(id);
 	}
 
 	public List<T> findAll() {
 		return repositorio.findAll();
 	}
 
+	public List<T> findAllActive() {
+		return repositorio.findAllAndSoftDeleteFalse();
+	}
+
 	public Page<T> findAll(Pageable pageable) {
 		return repositorio.findAll(pageable);
+	}
+
+	public Page<T> findAllSoftDeleteFalse(Pageable pageable) {
+		return repositorio.findAllAndSoftDeleteFalse(pageable);
 	}
 
 	public T edit(T t) {
@@ -44,5 +57,9 @@ public class BasicServiceImpl <T, ID, R extends JpaRepository<T, ID>>{
 
 	public boolean existById(ID id) {
 		return repositorio.existsById(id);
+	}
+
+	public boolean existByIdWhereSoftDeleteIsFalse(ID id) {
+		return this.repositorio.existByIdAndSoftDeleteFalse(id);
 	}
 }
