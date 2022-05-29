dfa0aea9c562bb3ac2a2b693b7dc42f3795bb0bf
acamus79 <adrianedgardocamus@gmail.com>
2022-05-29 19:28:23 -0300

    OT208-38
    Se agrega la Dependencia de Amazon Java SDK en el POM, en el aplication.properties se agregan las variables : aws.accessKeyId, aws.secretKey, aws.region, aws.endpointUrl, aws.bucketName, con los valores propios del almacenamiento de Amazon S3, se agrega un paquete config con una clase AwsConfig que aporta un Bean que retorna una instancia de Amazon S3 para lo cual utiliza las variables agregadas en el properties. Se crea un interface con los metodos para subir un archivo para obtenerlo y para listar.

---
+++ src/main/java/com/alkemy/ong/config/AwsConfig.java
@@ -0,0 +1,28 @@
+package com.alkemy.ong.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsConfig {
+
+    @Value("${aws.accessKey}")
+    private String accessKey;
+    @Value("${aws.secretKey}")
+    private String secretKey;
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public AmazonS3 getS3Client() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder.standard().withRegion(Regions.fromName(region))
+                .withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
+    }
+}
