d0565dd5b3f5a4ac83db14afa7ce047dbd4795bb
Martin <iriartemartin2@gmail.com>
2022-05-31 09:08:00 -0300

    Se eliminan los metodos de usercontroller que pertenecen a authorizationcontroller.

--- src/main/java/com/alkemy/ong/controllers/UserController.java
+++ src/main/java/com/alkemy/ong/controllers/UserController.java
@@ -1,22 +1,12 @@
 package com.alkemy.ong.controllers;
 
-import java.io.IOException;
-
-import javax.validation.Valid;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alkemy.ong.dto.request.user.UserLoginDto;
-import com.alkemy.ong.dto.request.user.UserRegisterDto;
-import com.alkemy.ong.exeptions.EmailNotSendException;
-import com.alkemy.ong.models.UserEntity;
-import com.alkemy.ong.services.EmailService;
 import com.alkemy.ong.services.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -26,35 +16,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserController {
 
+	public static final String DELETE_USER = "Usuario eliminado";
+	public static final String NO_DELETE_USER = "Usuario no eliminado, id incorrecto ó vacío";
 	private final UserService userService;
-	private final EmailService emailService;
 
-	@PostMapping("/login")
-	public ResponseEntity<String> login(@Valid @RequestBody UserLoginDto userLoginDto) {
+	@DeleteMapping("user/{id}")
+	public ResponseEntity<String> deleteUser(@PathVariable String id) {
 		try {
-			if (userService.findByEmail(userLoginDto.getEmail()).isPresent()) {
-				userService.login(userLoginDto);
-				// return token
-			} else
-				return ResponseEntity.notFound().build();
-		} catch (BadCredentialsException e) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("false");
+			return new ResponseEntity<>(this.userService.deleteUser(id) ? DELETE_USER : NO_DELETE_USER, HttpStatus.OK);
+		} catch (Exception ex) {
+			return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
 		}
-		return ResponseEntity.ok("ok");
-	}
-
-	@PostMapping("/register")
-	public ResponseEntity<UserEntity> signup(@RequestBody @Valid UserRegisterDto userDTO)
-			throws EmailNotSendException, IOException {
-//        userService.saveUser(userDTO);
-//        Authentication auth;
-//        auth = authenticationManager.authenticate(
-//                new UsernamePasswordAuthenticationToken(userDTO.getEmail(), userDTO.getPassword()));
-//        final String jwt = jwtTokenUtil.generateToken(auth);	
-//        return ResponseEntity.ok(new AuthResponseDTO(jwt));ww	
-		UserEntity user = userService.saveUser(userDTO);
-		emailService.sendEmailRegister(user.getEmail());
-
-		return ResponseEntity.status(HttpStatus.CREATED).body(user);
 	}
 }
