4c5050d42c5c24e35271937af445e6feb94ce48b
Martin <iriartemartin2@gmail.com>
2022-05-31 18:12:38 -0300

    Se agrega el envio de email de bienvenida en el registro y se agrega la exception de emailnotsend.

--- src/main/java/com/alkemy/ong/controllers/UserController.java
+++ src/main/java/com/alkemy/ong/controllers/UserController.java
@@ -1,6 +1,10 @@
 package com.alkemy.ong.controllers;
 
 import com.alkemy.ong.dto.request.user.UserRegisterDto;
+import com.alkemy.ong.exeptions.EmailNotSendException;
+
+import java.io.IOException;
+
 import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +12,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.*;
 import com.alkemy.ong.dto.request.user.UserLoginDto;
 import com.alkemy.ong.models.UserEntity;
+import com.alkemy.ong.services.EmailService;
 import com.alkemy.ong.services.UserService;
 import com.alkemy.ong.services.impl.UserServiceImpl;
 
@@ -22,6 +27,7 @@ public class UserController {
     public static final String NO_DELETE_USER = "Usuario no eliminado";
     private final UserServiceImpl userServiceImpl;
     private final UserService userService;
+    private final EmailService emailService;
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@Valid @RequestBody UserLoginDto userLoginDto) {
@@ -42,9 +48,11 @@ public class UserController {
      *
      * @param userDTO
      * @return String jwt Token
+     * @throws IOException 
+     * @throws EmailNotSendException 
      */
     @PostMapping("/register")
-    public ResponseEntity<UserEntity> signup(@RequestBody @Valid UserRegisterDto userDTO) {
+    public ResponseEntity<UserEntity> signup(@RequestBody @Valid UserRegisterDto userDTO) throws EmailNotSendException, IOException {
 //        userService.saveUser(userDTO);
 //        Authentication auth;
 //        auth = authenticationManager.authenticate(
@@ -52,6 +60,8 @@ public class UserController {
 //        final String jwt = jwtTokenUtil.generateToken(auth);
 //        return ResponseEntity.ok(new AuthResponseDTO(jwt));
         UserEntity user = userService.saveUser(userDTO);
+        emailService.sendEmailRegister(user.getEmail());
+        
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
