d7e270645484f936d46b11d8d8b9b16640006829
Martin <iriartemartin2@gmail.com>
2022-05-31 09:15:39 -0300

    Se crea el controlador de authenticationController, se pasan los metodos de usercontroller y se envia email en registro.

---
+++ src/main/java/com/alkemy/ong/controllers/AuthenticationController.java
@@ -0,0 +1,67 @@
+package com.alkemy.ong.controllers;
+
+import com.alkemy.ong.dto.request.user.UserLoginDto;
+import com.alkemy.ong.dto.request.user.UserRegisterDto;
+import com.alkemy.ong.exeptions.EmailNotSendException;
+import com.alkemy.ong.models.UserEntity;
+import com.alkemy.ong.services.EmailService;
+import com.alkemy.ong.services.UserService;
+import com.alkemy.ong.services.impl.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+	
+    private final UserServiceImpl userServiceImpl;
+    private final UserService userService;
+    private final EmailService emailService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@Valid @RequestBody UserLoginDto userLoginDto) {
+        try {
+            if (userServiceImpl.findByEmail(userLoginDto.getEmail()).isPresent()) {
+                userServiceImpl.login(userLoginDto);
+                // return token
+            }else
+                return ResponseEntity.notFound().build();
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("false");
+        }
+        return ResponseEntity.ok("ok");
+    }
+
+    /**
+     * SIGNUP Registration method that generates a token and an automatic login.
+     *
+     * @param userDTO
+     * @return String jwt Token
+     * @throws IOException 
+     * @throws EmailNotSendException 
+     */
+    @PostMapping("/register")
+    public ResponseEntity<UserEntity> signup(@RequestBody @Valid UserRegisterDto userDTO) throws EmailNotSendException, IOException {
+//        userService.saveUser(userDTO);
+//        Authentication auth;
+//        auth = authenticationManager.authenticate(
+//                new UsernamePasswordAuthenticationToken(userDTO.getEmail(), userDTO.getPassword()));
+//        final String jwt = jwtTokenUtil.generateToken(auth);
+//        return ResponseEntity.ok(new AuthResponseDTO(jwt));
+        UserEntity user = userService.saveUser(userDTO);
+        emailService.sendEmailRegister(user.getEmail());
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+}
