dcb59b8da3ed8782583d9bcdabd9051663d03429
Martin <iriartemartin2@gmail.com>
2022-06-02 08:23:29 -0300

    Se corrige el path de news a /news.

--- src/main/java/com/alkemy/ong/controllers/NewsController.java
+++ src/main/java/com/alkemy/ong/controllers/NewsController.java
@@ -4,8 +4,8 @@ import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alkemy.ong.dto.response.news.BasicNewsEntity;
@@ -17,14 +17,14 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("news")
+@RequestMapping("/news")
 public class NewsController {
 
 	private final NewsService newsService;
 	private final NewsMapper newsMapper;
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<BasicNewsEntity> getNews(@RequestParam String id){
+	public ResponseEntity<BasicNewsEntity> getNews(@PathVariable String id){
 		Optional<NewsEntity> newsEntity = newsService.findById(id);
 		
 		if(!newsEntity.isPresent()) {
