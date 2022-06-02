caa8c5609bd1449ee1c706adc75f6968971ab136
acamus79 <adrianedgardocamus@gmail.com>
2022-06-02 11:32:20 -0300

    OT208-56
    Methods are created in the service and the slide controller.

--- src/main/java/com/alkemy/ong/services/SlideService.java
+++ src/main/java/com/alkemy/ong/services/SlideService.java
@@ -5,8 +5,10 @@ import com.alkemy.ong.models.SlideEntity;
 
 import java.util.List;
 
-public interface SlideService extends BasicService<SlideEntity, String>{
+public interface SlideService extends BasicService<SlideEntity, String> {
 
     public List<SlideDto> getAll();
+
+    public SlideDto getSlide(String id);
 
 }
