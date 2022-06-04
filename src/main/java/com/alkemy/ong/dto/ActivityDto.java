c7b799ba1d8ef8b91ec302cd3557d8d8897428a9
Nestor Agredo <nagredo@bancolombia.com.co>
2022-06-04 15:36:56 -0500

    -Add ActivityController -Add ActivityDto -Add method create Activity

---
+++ src/main/java/com/alkemy/ong/dto/ActivityDto.java
@@ -0,0 +1,17 @@
+package com.alkemy.ong.dto;
+
+import lombok.Data;
+
+/**
+ * @author nagredo
+ * @project OT208-server
+ * @class ActivityDto
+ */
+@Data
+public class ActivityDto {
+    private String id;
+    private String name;
+    private String content;
+    private String image;
+
+}
