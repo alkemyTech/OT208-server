1e73a6e9bd3a54e8af550e5f219b893cf3326f3f
acamus79 <adrianedgardocamus@gmail.com>
2022-06-05 01:47:51 -0300

    Delete data.sql

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into roles (id, description , name , timestamps ) values
-('84849fce-a916-41dc-a8fc-e5664d10edfa','usuario', 'USER', null);
-
-insert into organizations (id, name , image, email , welcome_text , about_us_text , phone , timestamps , soft_delete ) values
-('7d254950-e45a-11ec-8fea-0242ac120002','SomosMas', 'Url' , 'somosmasong@mail.com',
-'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt a dolor ac scelerisque. Sed posuere, velit nec venenatis porta, purus est consequat arcu, sagittis lacinia nulla metus non lorem. Nulla tincidunt leo in nisl lacinia bibendum. Nulla non sollicitudin velit, nec hendrerit nisi. Integer ac odio ac ligula semper bibendum. Maecenas eget congue urna, tincidunt lacinia nisl. Sed dui ex, viverra eu tortor congue, condimentum consectetur nisl. Aenean at aliquam orci. ',
-'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt a dolor ac scelerisque. ',
-'01123456789', '2022-06-04T22:31:36+00:00' , 0);
