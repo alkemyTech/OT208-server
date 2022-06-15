f97ad4839ad046837851079c8ad6058dc81cb03b
DavidESanchezP <desanchez08@gmail.com>
2022-06-15 17:15:42 -0500

    Agregar paginación a Categorías

--- src/main/java/com/alkemy/ong/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/alkemy/ong/services/impl/CategoryServiceImpl.java
@@ -6,7 +6,13 @@ import com.alkemy.ong.models.CategoryEntity;
 import com.alkemy.ong.repositories.ICategoryRepository;
 import com.alkemy.ong.services.CategoryService;
 import com.alkemy.ong.services.mappers.CategoryMapper;
+import com.alkemy.ong.services.mappers.ObjectMapperUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,5 +39,24 @@ public class CategoryServiceImpl extends BasicServiceImpl<CategoryEntity,String,
 
         }
         return Optional.empty();
+    }
+
+    @Override
+    public Page<CategoryDetailDto> getCategories(Pageable pageable) {
+
+        List<CategoryEntity> categoryEntities = repository.findAll();
+        List<CategoryDetailDto> response;
+
+        if(!categoryEntities.isEmpty()){
+            response = ObjectMapperUtils.mapAll(categoryEntities,CategoryDetailDto.class);
+
+            final int star = (int)pageable.getOffset();
+            final int end = Math.min((star+pageable.getPageSize()),response.size());
+
+            return new PageImpl<>(response.subList(star,end),pageable,response.size());
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"There's not Category");
+        }
+
     }
 }
