8baab9f822adafc12aa2dc905d9e3b6ce523c283
acamus79 <adrianedgardocamus@gmail.com>
2022-06-19 01:03:39 -0300

    OT208-87
    I add Swagger OpenAPI 3.0 annotations in the Members controller

--- src/main/java/com/alkemy/ong/controllers/MemberController.java
+++ src/main/java/com/alkemy/ong/controllers/MemberController.java
@@ -6,6 +6,9 @@ import com.alkemy.ong.dto.response.members.MemberResponseDto;
 import com.alkemy.ong.exeptions.ValidationException;
 import com.alkemy.ong.models.MemberEntity;
 import com.alkemy.ong.services.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -29,12 +32,23 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok Get Page Members"),
+            @ApiResponse(responseCode = "403", description = "Forbidden Access Denied"),
+            @ApiResponse(responseCode = "404", description = "Not Found")})
+    @Operation(summary = "Get all members", description = "Get all members only if logged in as a User or as Administrator")
     public ResponseEntity<Page<MemberResponseDto>> getMembers(@PageableDefault(size = 10) Pageable pageable) {
         return ResponseEntity.ok(memberService.getMembers(pageable));
     }
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<MemberResponseDto> createMember(@Valid @RequestPart(name="dto") EntryMemberDto entryMemberDto, Errors errors, @RequestPart(name="img") MultipartFile file) {
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Member was successfully created"),
+            @ApiResponse(responseCode = "400", description = "There are validation errors or Required request parts are missing"),
+            @ApiResponse(responseCode = "403", description = "Forbidden Access Denied")})
+    @Operation(summary = "Create a new member", description = "Create a new member only if logged in as a User or as Administrator")
+    public ResponseEntity<MemberResponseDto> createMember(@Valid @RequestPart(name = "dto") EntryMemberDto entryMemberDto, Errors errors,
+                                                          @RequestPart(name = "img") MultipartFile file) {
         if (errors.hasErrors()) {
             throw new ValidationException(errors.getFieldErrors());
         }
@@ -45,13 +59,25 @@ public class MemberController {
     }
 
     @DeleteMapping("/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Member was successfully deleted"),
+            @ApiResponse(responseCode = "403", description = "Forbidden Access Denied"),
+            @ApiResponse(responseCode = "404", description = "Id is empty, or does not exist")})
+    @Operation(summary = "Delete member", description = "Delete a member by id, only if logged in as Administrator")
     public ResponseEntity<String> deleteMember(@PathVariable String id) {
         memberService.deleteMember(id);
-        return new ResponseEntity<>("Member was successfully deleted", HttpStatus.OK);
+        return new ResponseEntity<>("Member was successfully deleted", HttpStatus.NO_CONTENT);
     }
 
     @PutMapping(value = "/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<MemberResponseDto> updateMember(@Valid @RequestPart(name="dto") EditMemberDto editMemberDto, Errors errorsDto, @PathVariable String id, @Valid @RequestPart(name="img") MultipartFile file, Errors errorsFile) {
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Member was successfully updated"),
+            @ApiResponse(responseCode = "400", description = "There are validation errors or Required request parts are missing"),
+            @ApiResponse(responseCode = "403", description = "Forbidden Access Denied")})
+    @Operation(summary = "Update member", description = "Update a member by id, only if logged in as Administrator")
+    public ResponseEntity<MemberResponseDto> updateMember(@Valid @RequestPart(name = "dto") EditMemberDto editMemberDto, Errors errorsDto,
+                                                          @PathVariable String id,
+                                                          @Valid @RequestPart(name = "img") MultipartFile file, Errors errorsFile) {
         if (errorsDto.hasErrors()) {
             throw new ValidationException(errorsDto.getFieldErrors());
         }
@@ -61,14 +87,13 @@ public class MemberController {
         }
 
         Optional<MemberEntity> memberOptional = memberService.findById(id);
-        if (!memberOptional.isPresent()) {
-            ResponseEntity.notFound().build();
+        if (memberOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
         }
 
         if (!file.isEmpty()) {
             return ResponseEntity.ok(memberService.updateMember(editMemberDto, memberOptional.get(), file));
         }
-
         return ResponseEntity.ok(memberService.updateMember(editMemberDto, memberOptional.get()));
     }
 
