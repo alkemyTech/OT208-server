93efb42bd7c987fbdf80f78eb662f6b515b4eafd
acamus79 <adrianedgardocamus@gmail.com>
2022-06-26 20:39:03 -0300

    More Fixes
    BasicUserDto is corrected so that when displaying user information the password is not obtained.
    Validation Method is modified to give more functionality.

--- src/main/java/com/alkemy/ong/services/impl/ValidationServiceImpl.java
+++ src/main/java/com/alkemy/ong/services/impl/ValidationServiceImpl.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.services.impl;
 
 import com.alkemy.ong.models.RoleEntity;
+import com.alkemy.ong.security.enums.RolName;
 import com.alkemy.ong.services.ValidationService;
 import org.springframework.stereotype.Service;
 
@@ -8,22 +9,16 @@ import java.util.List;
 
 @Service
 public class ValidationServiceImpl implements ValidationService {
-//    @Override
-//    public boolean roleValidation(RoleForm roleForm) {
-//        return roleForm.getRol().equals(RolesEnum.USER.name()) || roleForm.getRol().equals(RolesEnum.ADMIN.name());
-//    }
 
     @Override
-    public boolean roleValidation(List<RoleEntity> role) {
+    public String roleValidation(List<RoleEntity> role) {
 
-        boolean flag = false;
-
-        for (RoleEntity roleEntity : role) {
-
-            if (roleEntity.getRolName().equals("USER") || roleEntity.getRolName().equals("ADMIN")) {
-                flag = true;
+        for (RoleEntity roleEntity : role)
+            if (roleEntity.getRolName().equals(RolName.ROLE_USER)) {
+                return "User " + roleEntity.getDescription();
+            } else if (roleEntity.getRolName().equals(RolName.ROLE_ADMIN)) {
+                return "Admin " + roleEntity.getDescription();
             }
-        }
-        return flag;
+        return "The user is not enabled";
     }
 }
